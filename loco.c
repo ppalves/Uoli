366725827b07034b983bdd650f5e3fe56766581a
Pupo <ppalves10@gmail.com>
2019-11-27 20:00:10 -0300

    acabou

--- loco.c
+++ loco.c
@@ -3,11 +3,10 @@
 //cabecalho das funcoes
 void print_int(int a);
 void sort_friends_order (int *vetor, int n, int *friends_order);
-void turn_to_head_direction(int direction, Vector3 angles);
+//void turn_to_head_direction(int direction, Vector3 angles);
 void turn_absolute_y(int angle, Vector3 angles);
-void dodge_enemy(int index, int current_axis);
 void wait(int waiting_time);
-int verify_enemies(int current_axis);
+//int verify_enemies(int current_axis);
 void print_int_with_new_line(int a);
 void verify_position(int *increaseX, int *increaseZ, Vector3 pos, int x, int z);
 void go_to_objective(int x, int z, Vector3 pos, Vector3 angles);
@@ -17,30 +16,25 @@ int verify_inclination(Vector3 angles);
 
 
 int main() {
-	set_head_servo(1, 78);
-    Vector3 pos;
-	Vector3 angles;
+	set_head_servo(1, 78);  //ajusta a cabeça levemente pra baixo para ver melhor montanhas
+    Vector3 pos; //inicia vetor usado pra guardar a posicao do GPS
+	Vector3 angles; //inicia vetor usado pra guardar os angulos do giroscopio
 	get_gyro_angles(&angles);
 	get_current_GPS_position(&pos);
-	print_int_with_new_line(pos.x);
-	print_int_with_new_line(pos.z);
 
-	int num_friends = sizeof(friends_locations)/sizeof(friends_locations[0]);
-	int order[num_friends];
-	me_and_the_boys(&order[0]);
+	int num_friends = sizeof(friends_locations)/sizeof(friends_locations[0]);   //numero de amigos
+	int order[num_friends];  //vetor com a ordem dos amigos
+	me_and_the_boys(&order[0]);  //calcula distancia e ordena o vetor
 
-	for (int i = 0; i < num_friends; i++){
+	for (int i = 0; i < num_friends; i++){   //loop para interar sobre os amigos ordenados e ir ate cada um deles
 		go_to_objective(friends_locations[order[i]].x, friends_locations[order[i]].z, pos, angles);
-		puts("cheguei\n");
 		wait(500);
-		print_int_with_new_line(get_time());
+		puts("Amigo encontrado!\n");
+		puts("Ola amigo!\n");
 	}
 
-	puts("terminei");
 	set_torque(0,0);
-	
-	
-	
+	puts("Todos os amigos encontrados, missao cumprida!\n");
 	
     while(1) {
 	//Nada
@@ -126,19 +120,14 @@ void go_to_objective(int x, int z, Vector3 pos, Vector3 angles){
 	int increaseX;
 	int increaseZ;
 	
-	verify_position(&increaseX, &increaseZ, pos, x, z);
+	verify_position(&increaseX, &increaseZ, pos, x, z); //verifica em qual eixo e sobre qual direcao precisa andar
 	
 	int angle;
 	int inimigo;
 	
-	while(increaseX != 2 || increaseZ != 2){
-		puts("increaseX: ");
-		print_int_with_new_line(increaseX);
-		puts("increaseZ: ");
-		print_int_with_new_line(increaseZ);
+	while(increaseX != 2 || increaseZ != 2){  //enquanto nao estiver perto do amigo verifica sobre qual eixo e em qual direcao precisa andar
 
 		if (increaseX != 2){
-			puts("andando no X\n");
 			int increaseX_inicial =  increaseX;
 		
 			if (increaseX)
@@ -150,27 +139,21 @@ void go_to_objective(int x, int z, Vector3 pos, Vector3 angles){
 			
 			set_torque(7,7);
 			
+			//while que executa enquanto o Uoli anda sobre o eixo, quebra o loop caso encontre algum obstaculo/inclinacao ou caso ele ja tenha passado da coordenada do amigo
 			while(!verify_obstacles() && increaseX_inicial == increaseX && verify_inclination(angles)){
-				verify_position(&increaseX, &increaseZ, pos, x, z);
+				verify_position(&increaseX, &increaseZ, pos, x, z); //verifica toda hora se ele ja nao passou do amigo e portanto precisa andar no outro eixo
 				get_gyro_angles(&angles);
-				if ((angles.y-angle)<-30) {
+				if ((angles.y-angle)<-30) {  //caso esteja indo em uma direcao muito errada da break
 					if((angles.y-angle)>30) {
-						puts("lala");
 						break;
 					}
 				}
-			}
-			puts("vai bater\n");
+			}			
 			
-			
-			
-			set_torque(0,0);
-			//wait(3);
-			
+			set_torque(0,0);	
 		}
 
 		if(increaseZ != 2){
-			puts("andando no Z\n");
 			int increaseZ_inicial = increaseZ;
 
 			if (increaseZ)
@@ -178,43 +161,52 @@ void go_to_objective(int x, int z, Vector3 pos, Vector3 angles){
 			else if (!increaseZ)
 				angle = 180;
 			
-			
 			turn_absolute_y(angle, angles);
 			
 			set_torque(7,7);
-			
+
+			//while que executa enquanto o Uoli anda sobre o eixo, quebra o loop caso encontre algum obstaculo/inclinacao ou caso ele ja tenha passado da coordenada do amigo
 			while (!verify_obstacles() && increaseZ_inicial == increaseZ && verify_inclination(angles)){
-				verify_position(&increaseX, &increaseZ, pos, x, z);
+				verify_position(&increaseX, &increaseZ, pos, x, z); //verifica toda hora se ele ja nao passou do amigo e portanto precisa andar no outro eixo
 				get_gyro_angles(&angles);
-				if ((angles.y-angle)<-30) {
+				if ((angles.y-angle)<-30) { //caso esteja indo em uma direcao muito errada da break
 					if((angles.y-angle)>30) {
-						puts("lala");
 						break;
 					}
 				}
-			}
-			puts("vai bater\n");
-			
+			}			
 			
 			set_torque(0,0);
-			//wait(3);
-			
-
 		}
 
-		verify_position(&increaseX, &increaseZ, pos, x, z);
-
+		verify_position(&increaseX, &increaseZ, pos, x, z);  //verifica em qual eixo e sobre qual direcao precisa andar
 	}
-	
-	
-	
 }
 
+/*
+ *Função para printar quebra de linha apos printar um numero inteiro
+ *
+ * Parametros:
+ * 		int a: inteiro a ser printado
+ * 
+ * Retorno:
+ * 		void
+ */
 void print_int_with_new_line(int a){
 	print_int(a);
 	puts("\n");
 }
 
+
+/*
+ *Função recursiva para passar inteiros para char para printar
+ *
+ * Parametros:
+ * 		int a: inteiro a ser printado
+ * 
+ * Retorno:
+ * 		void
+ */
 void print_int(int a) {
 	if (a < 0){
 		a = a * (-1);
@@ -239,6 +231,15 @@ void print_int(int a) {
     }
 }
 
+/*
+ *Função para ordenar os amigos em ordem de proximidade. Calcula as distancias e chama outra funcao para ordenar 
+ *
+ * Parametros:
+ * 		int *friends_order: Ponteiro para o vetor que sera usado para ordenar os index dos amigos
+ * 
+ * Retorno:
+ * 		void
+ */
 void me_and_the_boys(int *friends_order) {
     int num_friends = sizeof(friends_locations)/sizeof(friends_locations[0]);
     int dist_aux[num_friends];
@@ -254,25 +255,18 @@ void me_and_the_boys(int *friends_order) {
     sort_friends_order(dist_aux, num_friends, friends_order);
 }
 
-void turn_to_head_direction(int direction, Vector3 angles){
-	get_gyro_angles(&angles);
-	int absolute_angle = (direction - 78) + angles.y;
-	print_int(absolute_angle);
-	turn_absolute_y(absolute_angle, angles);
-	return;
-}
 
-void turn_absolute_y(int angle, Vector3 angles){
-	set_torque(20, -20);
-	while(1){
-		get_gyro_angles(&angles);
-		if(angles.y >= angle - 5 && angles.y <= angle + 5)
-			break;
-	}
-	set_torque(0,0);
-	return;
-}
-
+/*
+ *Função para ordenar o vetor de distancias dos amigos
+ *
+ * Parametros:
+ * 		int *vetor: vetor com as distancias
+ * 		int n: numero de amigos
+ * 		int *friends_order: vetor para salvar a ordem com base nos index dos amigos
+ * 
+ * Retorno:
+ * 		void
+ */
 void sort_friends_order (int *vetor, int n, int *friends_order) {
     int k, j, aux, aux_2;
     for (k = 1; k < n; k++) {
@@ -289,38 +283,37 @@ void sort_friends_order (int *vetor, int n, int *friends_order) {
     }
 }
 
-int verify_enemies(int current_axis) {
-    int num_enemies = sizeof(dangerous_locations)/sizeof(dangerous_locations[0]);
-    Vector3 pos;
-    for(int i=0; i<num_enemies; i++) {
-		get_current_GPS_position(&pos);
-		int dist = (pos.x-dangerous_locations[i].x)*(pos.x-dangerous_locations[i].x);
-		dist = dist + (pos.z-dangerous_locations[i].z)*(pos.z-dangerous_locations[i].z);
-		if (dist < 100) {
-			return 1;
-			// if (current_axis == 0) {
-			// 	if (pos.z<(dangerous_locations[i].z) && pos.z>(dangerous_locations[i].z-25)) {
-			// 		return 1;
-			// 	}
-			// 	else if (pos.z>(dangerous_locations[i].z) && pos.z<(dangerous_locations[i].z+25)) {
-			// 		return 1;
-			// 	}
-			// }
-			// else {
-			// 	if (pos.x<(dangerous_locations[i].x) && pos.x>(dangerous_locations[i].x-25)) {
-			// 		return 1;
-			// 	}
-			// 	else if (pos.x>(dangerous_locations[i].x) && pos.x<(dangerous_locations[i].x+25)) {
-			// 		return 1;
-			// 	}
-			// }
-		}
-    }
-	return 0;
+/*
+ *Função para girar o Uoli para um angulo absoluto
+ *
+ * Parametros:
+ * 		int angle: angulo a ser girado
+ * 		Vector3 angles: vetor com os angulos de euler do Uoli
+ * 
+ * Retorno:
+ * 		void
+ */
+void turn_absolute_y(int angle, Vector3 angles){
+	set_torque(20, -20);
+	while(1){    //loop ate o Uoli atingir aproximadamente o angulo desejado
+		get_gyro_angles(&angles);
+		if(angles.y >= angle - 5 && angles.y <= angle + 5)
+			break;
+	}
+	set_torque(0,0);
+	return;
 }
 
 
-
+/*
+ *Função que utiliza do GPT para esperar um tempo
+ *
+ * Parametros:
+ * 		int waiting_time: tempo a ser esperado em milisegundos
+ * 
+ * Retorno:
+ * 		void
+ */
 void wait(int waiting_time) {
     
     int initial_time = get_time();
@@ -329,6 +322,16 @@ void wait(int waiting_time) {
     }
 }
 
+/*
+ *Função para verificar a presença de obstaculos a frente
+ *
+ * Parametros:
+ * 		none
+ * 
+ * Retorno:
+ * 		int: 0 nenhum obstaculo
+ * 			 1 obstaculo a frente
+ */
 int verify_obstacles() {
     int distance = get_us_distance();
     if (distance == 65535) {
@@ -340,5 +343,54 @@ int verify_obstacles() {
 }
 
 
+/*
+ *Função para virar o Uoli para onde o sensor ultrasonico estiver apontando.
+ *Nao utilizamos a funcao pois nao foi necessaria no codigo final
+ */
+
+/*
+void turn_to_head_direction(int direction, Vector3 angles){
+	get_gyro_angles(&angles);
+	int absolute_angle = (direction - 78) + angles.y;
+	print_int(absolute_angle);
+	turn_absolute_y(absolute_angle, angles);
+	return;
+}
+*/
 
 
+/*Função não utilizada. A ideia da função era verificar se era possivel passar por um inimigo, caso contrario desviar mudando de eixo
+ *mesma forma usada pra desviar dos outros obstaculos
+ *Não conseguimos implementar a função de um jeito satisfatorio e muitas vezes o Uoli ficava preso girando no lugar, sendo assim a funcao nao foi utilizada
+ */
+/*
+int verify_enemies(int current_axis) {
+    int num_enemies = sizeof(dangerous_locations)/sizeof(dangerous_locations[0]);
+    Vector3 pos;
+    for(int i=0; i<num_enemies; i++) {
+		get_current_GPS_position(&pos);
+		int dist = (pos.x-dangerous_locations[i].x)*(pos.x-dangerous_locations[i].x);
+		dist = dist + (pos.z-dangerous_locations[i].z)*(pos.z-dangerous_locations[i].z);
+		if (dist < 100) {
+			return 1;
+			if (current_axis == 0) {
+				if (pos.z<(dangerous_locations[i].z) && pos.z>(dangerous_locations[i].z-25)) {
+					return 1;
+				}
+				else if (pos.z>(dangerous_locations[i].z) && pos.z<(dangerous_locations[i].z+25)) {
+					return 1;
+				}
+			}
+			else {
+				if (pos.x<(dangerous_locations[i].x) && pos.x>(dangerous_locations[i].x-25)) {
+					return 1;
+				}
+				else if (pos.x>(dangerous_locations[i].x) && pos.x<(dangerous_locations[i].x+25)) {
+					return 1;
+				}
+			}
+		}
+    }
+	return 0;
+}
+*/
